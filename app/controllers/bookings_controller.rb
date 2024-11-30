7d2cc3b00eb4c35949641d3c5d567f16c5695243
DasTapan <tapands.das@gmail.com>
2024-11-30 13:20:35 +0530

    Add bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,15 @@
 class BookingsController < ApplicationController
     def new
+        @tickets = booking_params[:tickets].to_i
+        @flight = Flight.find(booking_params[:flight_id])
+    end
+
+    private
+
+    def booking_params 
+        params.require(:booking).permit(
+            :tickets,
+            :flight_id
+        )
     end
 end

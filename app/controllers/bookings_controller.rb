b5e8ef6ec3236fb8fe46b3e975bac4593eeaf6a8
DasTapan <tapands.das@gmail.com>
2024-12-03 18:59:23 +0530

    Book ticket by creating passengers

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,14 +2,42 @@ class BookingsController < ApplicationController
     def new
         @tickets = booking_params[:tickets].to_i
         @flight = Flight.find(booking_params[:flight_id])
+        @booking = Booking.new
+        @tickets.times {@booking.passengers.build}
     end
 
+    def show
+        @booking = Booking.find(params[:id])
+        @flight = @booking.flight
+        @departure_city = @flight.departure_airport[:city]
+        @arrival_city = @flight.arrival_airport[:city]
+        @flight_on = @flight.start.strftime("%d-%b-%Y")
+        @passengers = @booking.passengers
+        @duration = @flight[:duration]
+    end
+    
+    def create
+        booking_data = booking_params.except(:tickets)
+        @booking = Booking.new(booking_data)   
+        
+        if @booking.save
+            redirect_to @booking
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+    
     private
 
     def booking_params 
         params.require(:booking).permit(
             :tickets,
-            :flight_id
+            :flight_id,
+            passengers_attributes: [
+                :name,
+                :mobile,
+                :email
+            ]
         )
     end
 end
